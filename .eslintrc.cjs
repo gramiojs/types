9137383b9bc1d258d22f63ac9a1b921827ccd3b2
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-01-05 20:04:55 +0300

    chore: add linters, gitignore, prettier and package.json improves

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+const { configure, presets } = require("eslint-kit");
+
+module.exports = configure({
+    extend: {
+        rules: {
+            "@typescript-eslint/no-namespace": "off"
+        }
+    },
+    presets: [
+        presets.imports(),
+        presets.node(),
+        presets.prettier(),
+        presets.typescript(),
+    ],
+});
