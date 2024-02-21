5197fa3354ba588f4b53ed17f75079cf66675c2f
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-02-21 20:34:42 +0300

    fix: generate-changelog at CICD

--- scripts/generate-changelog.ts
+++ scripts/generate-changelog.ts
@@ -1,11 +1,13 @@
 import { execSync } from "node:child_process";
-import { writeFileSync } from "node:fs";
+import { randomUUID } from "node:crypto";
+import { appendFileSync } from "node:fs";
 import { EOL } from "node:os";
 
 function getLatestTag() {
 	try {
 		return execSync("git describe --abbrev=0 --tags").toString().trim();
 	} catch (e) {
+		console.warn(e);
 		return execSync("git rev-list --max-parents=0 HEAD").toString().trim();
 	}
 }
@@ -20,7 +22,15 @@ const commits = execSync(
 
 console.log(getLatestTag(), commits);
 
-writeFileSync(
-	process.env.GITHUB_OUTPUT!,
-	`changelog="${commits.join(EOL.repeat(2))}"${EOL}`,
-);
+const version = execSync("npm pkg get version").toString().replace(/"/gi, "");
+
+const delimiter = `---${randomUUID()}---${EOL}`;
+
+if (process.env.GITHUB_OUTPUT)
+	appendFileSync(
+		process.env.GITHUB_OUTPUT,
+		`changelog<<${delimiter}${commits.join(
+			EOL.repeat(2),
+		)}${EOL}${delimiter}version=${version}${EOL}`,
+	);
+else console.log("Not github actions");
