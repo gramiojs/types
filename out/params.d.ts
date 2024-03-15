95e144a65c9df405c2949bd0fc00b11a7a10aee3
kravetsone <kravetsone@users.noreply.github.com>
2024-03-15 16:18:16 +0000

    release v

--- out/params.d.ts
+++ out/params.d.ts
@@ -1,6 +1,6 @@
 /**
  * Based on Bot API v7.1.0 (16.2.2024)
- * Generated at 3/15/2024, 3:14:22 PM using [types](https://github.com/gramiojs/types) and [schema](https://ark0f.github.io/tg-bot-api) generators
+ * Generated at 3/15/2024, 4:18:15 PM using [types](https://github.com/gramiojs/types) and [schema](https://ark0f.github.io/tg-bot-api) generators
  */
 import * as Objects from "./objects"
 
