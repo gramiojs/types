24da24bbccc6918a9b87e0aa33d220d2b08491df
kravetsone <kravetsone@users.noreply.github.com>
2024-03-15 09:13:58 +0000

    release v

--- out/objects.d.ts
+++ out/objects.d.ts
@@ -1,6 +1,6 @@
 /**
  * Based on Bot API v7.1.0 (16.2.2024)
- * Generated at 3/15/2024, 8:17:59 AM using [types](https://github.com/gramiojs/types) and [schema](https://ark0f.github.io/tg-bot-api) generators
+ * Generated at 3/15/2024, 9:13:56 AM using [types](https://github.com/gramiojs/types) and [schema](https://ark0f.github.io/tg-bot-api) generators
  */
 
 /**
