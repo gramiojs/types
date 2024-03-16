d5266518e9969c1046e2d56b630927c7696ad41a
kravetsone <kravetsone@users.noreply.github.com>
2024-03-16 02:19:15 +0000

    release v

--- out/methods.d.ts
+++ out/methods.d.ts
@@ -1,6 +1,6 @@
 /**
  * Based on Bot API v7.1.0 (16.2.2024)
- * Generated at 3/16/2024, 1:50:29 AM using [types](https://github.com/gramiojs/types) and [schema](https://ark0f.github.io/tg-bot-api) generators
+ * Generated at 3/16/2024, 2:19:13 AM using [types](https://github.com/gramiojs/types) and [schema](https://ark0f.github.io/tg-bot-api) generators
  */
 import {
     CallAPIWithOptionalParams,
