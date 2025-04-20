784adc0f1cf56a46bfb44a1b42455f4648daee86
kravetsone <kravetsone@users.noreply.github.com>
2025-04-20 08:23:03 +0000

    release v9.0.2

--- out/index.d.ts
+++ out/index.d.ts
@@ -10,7 +10,7 @@
  *
  * Based on Bot API v9.0.0 (11.04.2025)
  *
- * Generated at 13.04.2025, 15:19:06 using [types](https://github.com/gramiojs/types) and [schema](https://ark0f.github.io/tg-bot-api) generators
+ * Generated at 20.04.2025, 08:22:13 using [types](https://github.com/gramiojs/types) and [schema](https://ark0f.github.io/tg-bot-api) generators
  */
 
 export type * from "./methods"
--- out/methods.d.ts
+++ out/methods.d.ts
@@ -13,7 +13,7 @@
  *
  * Based on Bot API v9.0.0 (11.04.2025)
  *
- * Generated at 13.04.2025, 15:19:06 using [types](https://github.com/gramiojs/types) and [schema](https://ark0f.github.io/tg-bot-api) generators
+ * Generated at 20.04.2025, 08:22:13 using [types](https://github.com/gramiojs/types) and [schema](https://ark0f.github.io/tg-bot-api) generators
  */
 
 import type {
@@ -798,7 +798,7 @@ export interface APIMethods {
      */
     readBusinessMessage: CallAPI<Params.ReadBusinessMessageParams, true>
     /**
-     * Delete messages on behalf of a business account. Requires the *can\_delete\_outgoing\_messages* business bot right to delete messages sent by the bot itself, or the *can\_delete\_all\_messages* business bot right to delete any message. Returns *True* on success.
+     * Delete messages on behalf of a business account. Requires the *can\_delete\_sent\_messages* business bot right to delete messages sent by the bot itself, or the *can\_delete\_all\_messages* business bot right to delete any message. Returns *True* on success.
      *
      * [Documentation](https://core.telegram.org/bots/api/#deletebusinessmessages)
      */
