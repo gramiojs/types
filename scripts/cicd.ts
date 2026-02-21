6de8640004573da2242a151db0e9394b073817ba
kravetsone <57632712+kravetsone@users.noreply.github.com>
2026-02-21 21:06:21 +0300

    chore: migrate to @gramio/schema-parser and remove tg-bot-api dependency

--- scripts/cicd.ts
+++ scripts/cicd.ts
@@ -2,12 +2,13 @@ import { execSync } from "node:child_process";
 import { createHash } from "node:crypto";
 import fs from "node:fs/promises";
 import { EOL } from "node:os";
+import { getCustomSchema } from "@gramio/schema-parser";
 import { version } from "../package.json";
-import { SCHEMA_FILE_PATH } from "../src/config";
-import { IBotAPI } from "../src/types";
 
-const schemaFile = await fs.readFile(SCHEMA_FILE_PATH);
-const hash = createHash("sha1").update(schemaFile).digest("hex");
+const schema = await getCustomSchema();
+const hash = createHash("sha1")
+	.update(JSON.stringify(schema.version))
+	.digest("hex");
 
 const previousHash = await fs.readFile("./hash.txt").then(String);
 if (previousHash === hash) {
@@ -15,13 +16,11 @@ if (previousHash === hash) {
 	process.exit(1);
 }
 
-const schema = JSON.parse(String(schemaFile)) as IBotAPI.ISchema;
-
 const [major, minor] = version.split(".").map(Number);
 
 if (major !== schema.version.major || minor !== schema.version.minor) {
 	execSync(
-		`npm pkg set version=${schema.version.major}.${schema.version.minor}.${schema.version.patch}`,
+		`npm pkg set version=${schema.version.major}.${schema.version.minor}.0`,
 	);
 } else execSync("npm version patch --no-git-tag-version");
 
